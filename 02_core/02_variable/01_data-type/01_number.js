d5b0e35fa64797a20f5defb34e25f0fdbf7b94d1
hyoju777 <cladius7777@naver.com>
2024-05-23 18:50:44 +0900

    240523

---
+++ 02_core/02_variable/01_data-type/01_number.js
@@ -0,0 +1,35 @@
+/*
+
+    자바의 경우 정수와 실수를 구분해 int,long,float,dobule 등과 같은 다양한 숫자 타입을 제공한다.
+    
+    하지만 자바스크립트의 경우 하나의 숫자 타입만 존재하고 모든 수를 실수를 처리한다.
+    */
+   // 자바의 변수 표현식
+   // 자료형 변수명 = 10;
+   
+   Number();
+
+    var integer =10;
+    var doblue =5.5;
+    var negative = -10;
+    var int = 10;
+
+    console.log
+    console.lot(number("10) * "10");
+    console.log(typeof integer);
+    console.log(typeof dobule);
+    console.log(typeof negative);
+
+    console.log(10 === 10.0);
+    console.log("test :" +(10 == '10'));
+
+    consonle.log(typeof(10/4));
+
+    /*
+        Infinity : 양의 무한대
+        -Infinity : 음의 무한대
+        NaN : 산술 연산 불가
+        */
+    console.log(10/0);
+    console.log(10/-10);
+    console.log(1*문자열');
